c7b9793c1ff4f19f8b537f21d3d79f2d5bbb7ce2
vxu4 <vixixu68@gmail.com>
2020-11-16 18:57:56 -0500

    added two mock charts

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 import StatusGraph from './components/caseStatus';
-import BodyCam from './components/caseStatus';
-import NoBodyCam from './components/caseStatus';
+import BodyCam from './components/bodycam';
+import NoBodyCam from './components/noBodycam';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Mapping</h1>
       <StatusGraph />
-      <div className="d-flex flex-row">
+      <div className="d-flex flex-row justify-content-center align-items-center">
         <BodyCam /> 
-        <NoBodyCam /> 
+        <NoBodyCam />
       </div>
     </div>
   );
--- my-app/src/components/bodycam.jsx
+++ my-app/src/components/bodycam.jsx
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
+// import Chart from 'chart.js';
+var Chart = require("chart.js");
+
 
 class BodyCam extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.node = React.createRef();
+  }
+  componentDidMount() {
+    const node = this.node;
+
+    var myChart = new Chart(node, {
+      type: "bar",
+      data: {
+        labels: ["Red", "Blue", "Yellow"],
+        datasets: [
+          {
+            label: "# of Cases",
+            data: [12, 19, 3],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)"
+            ]
+          }
+        ]
+      }
+    });
   }
 
   render() {
     return (
-      <div className="">
+      <div class="chart-container" style={{position: 'relative', width: '35vw'}}>
+        <canvas
+          ref={node => (this.node = node)}
+        />
       </div>
-    )
+    );
   }
+
 }
 
 export default BodyCam;
