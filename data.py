da252b03e20f0bb1f94e5f29270e1c94ba9788ff
Nisha Khater <nisha_khater@brown.edu>
2020-11-16 17:51:50 -0500

    Update data.py

--- data.py
+++ data.py
@@ -3,9 +3,10 @@ import sqlite3
 conn = sqlite3.connect('police_data.db')
 c = conn.cursor()
 
-race = ('Black',)
-c.execute('SELECT COUNT(*) FROM police_violence_2017 WHERE race=?', race)
-print(c.fetchone()) # prints the number of victims who were Black
+def get_race_nums(race):
+    c.execute('SELECT COUNT(*) FROM police_violence_2017 WHERE race=?', race)
+    return c.fetchone() # prints the number of victims who were Black
 
-c.execute('SELECT name FROM police_violence_2017 WHERE race=?', race)
-print(c.fetchall()) # prints the names of victims who were Black
+def get_race_names(race):
+    c.execute('SELECT name FROM police_violence_2017 WHERE race=?', race)
+    return c.fetchall() # prints the names of victims who were Black
