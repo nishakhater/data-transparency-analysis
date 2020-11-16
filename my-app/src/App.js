afea0c2f886fb5d17b0a12a8601bc7f72bebdc8f
vxu4 <vixixu68@gmail.com>
2020-11-16 18:20:04 -0500

    adding

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import StatusGraph from './components/caseStatus';
+import Body from './components/caseStatus';
+import StatusGraph from './components/caseStatus';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Mapping</h1>
+      <StatusGraph />
+      <div className="d-flex flex-row">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
